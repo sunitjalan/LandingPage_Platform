cc46be59f196b138f8dcf7b9b04f7e88f870f436
tjain18 <tj18games@gmail.com>
2022-08-31 17:40:45 +0530

    commit

---
+++ public/main.js
@@ -0,0 +1,28 @@
+(() => {
+    const hours = Selector(".hours");
+    const minutes = Selector(".minutes");
+    const seconds = Selector(".seconds");
+    const container = Selector(".container");
+    const checkbox = document.getElementById("switch");
+    const img = document.getElementById("shops");
+
+    setInterval(() => {
+        const time = new Date();
+        hours.textContent = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
+        minutes.textContent = time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
+        seconds.textContent = time.getSeconds() < 10 ? `0${time.getSeconds()}` : time.getSeconds();
+    }, 1000)
+
+    checkbox.onchange = (event) => {
+        if (event.target.checked) {
+            img.src = "./assets/Night_5.png";
+        }
+        else {
+            img.src = "./assets/Night_4.png";
+        }
+    }
+})();
+
+function Selector(element) {
+    return document.querySelector(element);
+}
